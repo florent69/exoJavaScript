003b26ddb432133acbf41e421ba42ea3f3a0336b
florent69 <florent.ta@le-campus-numerique.fr>
2019-04-01 16:44:04 +0200

    Initial commit  Changes to be committed:    new file:   README.MD    exo-0/index.html    exo-1/index.html    exo-1/script.js

---
+++ exo-1/script.js
@@ -0,0 +1,7 @@
+// you can write js here
+console.log('hello from file');
+var kelvin = 294;
+var celsius = kelvin - 273; // la variable celsius est égale à 294 - 273
+console.log(celsius); // Dans ce cas 21
+var fahrenheit = celsius * (9/2) +32;
+console.log(fahrenheit); // Dans ce cas 21
