dd418e0ef0ffd943f8af7877eb7126840c4d9c35
florent69 <florent.ta@le-campus-numerique.fr>
2019-04-01 16:57:38 +0200

    Exo-1 fini

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,7 +1,8 @@
 // you can write js here
 console.log('hello from file');
-var kelvin = 294;
+var kelvin = prompt('Quelle est la température en Kelvin aujourd\'hui ?');
 var celsius = kelvin - 273; // la variable celsius est égale à 294 - 273
 console.log(celsius); // Dans ce cas 21
-var fahrenheit = celsius * (9/2) +32;
-console.log(fahrenheit); // Dans ce cas 21
+var fahrenheit = celsius * (9/2) +32; // fahrenheit = celsius multiplié par 9 divisé par 2 plus 32
+console.log(fahrenheit); // Affiche la variable fahrenheit
+console.log(Math.floor(fahrenheit)); // Arrondi la variable fahrenheit
