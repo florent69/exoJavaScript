2eff05ae1a657d2cc2b674a0b39c8359476c1708
florent69 <florent.ta@le-campus-numerique.fr>
2019-04-02 17:02:31 +0200

    exo-5 fin de journée mardi

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,13 @@
 // you can write js here
 
 console.log('exo-5');
+
+var input = prompt('La baleine répetera en aeie !');
+var chars = input.split('');
+
+var voyels = [A, E, I, O, U, Y];
+var resultArray = [];
+
+for (i=0; i=input.length; i++) {
+
+}
