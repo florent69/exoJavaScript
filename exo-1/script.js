f207af4c321d92936583efc26ee39eb005ea5c7b
florent69 <florent.ta@le-campus-numerique.fr>
2019-04-02 11:47:16 +0200

    exo-2 fini

--- exo-1/script.js
+++ exo-1/script.js
@@ -3,6 +3,6 @@ console.log('hello from file');
 var kelvin = prompt('Quelle est la température en Kelvin aujourd\'hui ?');
 var celsius = kelvin - 273; // la variable celsius est égale à 294 - 273
 console.log(celsius); // Dans ce cas 21
-var fahrenheit = celsius * (9/2) +32; // fahrenheit = celsius multiplié par 9 divisé par 2 plus 32
+var fahrenheit = celsius * (9/5) +32; // fahrenheit = celsius multiplié par 9 divisé par 2 plus 32
 console.log(fahrenheit); // Affiche la variable fahrenheit
 console.log(Math.floor(fahrenheit)); // Arrondi la variable fahrenheit
--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,27 @@
 // you can write js here
 console.log('exo-2');
+var age = 10;
+var raceNumber = 0;
+var name = 'Tom';
+var register = name || 'non-register';
+
+if (register === 'non-register'){
+    raceNumber = Math.floor(Math.random()*1000);
+    console.log(raceNumber);
+} else {                             // if else normal //
+    raceNumber ++;
+    console.log(name + ' has the race n° ' + raceNumber);
+}
+
+// register !== 'non-register' ? console.log(name + ' has the race n° ' + raceNumber ++) : console.log(Math.floor(Math.random() * 1000));
+// Le raceNumber s'affiche et s'incrémente APRES !! L'utilisation d'un Ternary se fait uniquement pour un if else simple.
+
+if (age > 18 && register !== 'non-register') {
+    console.log('You will race at 9:30am');
+} else if (age >= 18 || register !== 'non-register') {
+    console.log('You will race at 11:00 am : ' + raceNumber);
+} else if (age < 18 || register == 'non-register') {
+    console.log('You will race at 12:30 pm : ' + raceNumber);
+} else {
+    console.log('Go to see the registration desk');
+}
