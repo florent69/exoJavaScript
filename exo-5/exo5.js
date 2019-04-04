47d774255255f7dea2dce165e972dc036f4ae2eb
florent69 <florent.ta@le-campus-numerique.fr>
2019-04-04 13:07:00 +0200

    exo-5 fini

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -3,7 +3,7 @@
 console.log('exo-5');
 
 var input = prompt('La baleine répetera en aeie !');
-//var input = input.split('');
+//var input = input.split(''); // Met tous les caractères dans un tableau //
 console.log(input);
 var voyels = ['a', 'e', 'i', 'o', 'u', 'y'];
 var resultArray = [];
@@ -12,9 +12,14 @@ for (i=0 ; i < input.length ; i++) {
     // console.log(input[i]);
     var indexOfInput = voyels.indexOf(input[i]);
     // console.log(indexOfInput);
-    if (indexOfInput > -1) {
-        resultArray.push(input[i]);
+    voyels.forEach(function(voyel){
+        if (voyel === input[i]) {
+            resultArray.push(input[i]);
+        }
+    });
+    // if (indexOfInput > -1) {
+    //     resultArray.push(input[i]);
        // resultArray.toUpperCase();
-    }
+    // }
 }
 console.log(resultArray.join(' ').toUpperCase());
