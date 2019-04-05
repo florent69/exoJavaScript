ace430c3568d1219e610032a5962ace499d87bd9
florent69 <florent.ta@le-campus-numerique.fr>
2019-04-05 17:05:13 +0200

    fin de semaine

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -8,18 +8,23 @@ console.log(input);
 var voyels = ['a', 'e', 'i', 'o', 'u', 'y'];
 var resultArray = [];
 
-for (i=0 ; i < input.length ; i++) {
-    // console.log(input[i]);
-    var indexOfInput = voyels.indexOf(input[i]);
-    // console.log(indexOfInput);
-    voyels.forEach(function(voyel){
-        if (voyel === input[i]) {
+// for (i = 0; i < input.length; i++) {     // Double boucle
+//     // console.log(input[i]);
+//     voyels.forEach(function (voyel) {
+//         if (voyel === input[i]) {
+//             resultArray.push(input[i]);
+//         }
+//     })
+// }
+// ;
+
+for (i = 0; i < input.length; i++) {
+    var indexOfInput = voyels.indexOf(input[i]);     // renvoie l'index de la lettre
+    //console.log(indexOfInput);
+        if (indexOfInput > -1) {
             resultArray.push(input[i]);
         }
-    });
-    // if (indexOfInput > -1) {
-    //     resultArray.push(input[i]);
-       // resultArray.toUpperCase();
-    // }
 }
+;
+
 console.log(resultArray.join(' ').toUpperCase());
--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,26 @@
 console.log("exo-7");
 
+var trad = {
+    "car": "Voiture",
+    "house": "Maison",
+    "game": "Jeux",
+    "videoGame": "Jeux Videos",
+    "show": "Spectacle"
+}
+// Object.keys pour mettre les clés dans un tableau
+var tradKeys = Object.keys(trad);
+// On rajoute une propriété à chaque objet de jsonDatas
+jsonDatas.forEach(function (data) {  // on parcourt le tableau jsonData, on nomme les objets data
+    for (i = 0; i < tradKeys.length; i++) {    // on parcourt le tableau tradKeys sur sa longueur
+        if (tradKeys[i] === data.type) {       // si l'index i de tradKeys === au type d'une data
+            data.typeTrad = trad[tradKeys[i]]; // on crée une nouvelle propriété dans l'objet qui prend la valeur de tradKeys
+        }
+    }
+});
+
 console.log(jsonDatas);
+
+function show() {
+    var getSelect = document.getElementById("typeJsD").value;
+    console.log( getSelect);
+}
